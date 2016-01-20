0721028288b62cdf245420a85b631cf49ce6746e
James Ward <james@jamesward.org>
2016-01-20 15:17:18 -0700

    runs args differently

--- test/test.sh
+++ test/test.sh
@@ -9,16 +9,32 @@ SBT_EXEC=$(realpath $1)
 run_test() {
   local DIR=$1
   local CMD=$2
-  local EXPECTED=$3
-  local IN=$4
+  local ARGS=$3
+  local EXPECTED=$4
+  local IN=$5
 
   local OLD_PWD=$PWD
   cd $DIR
 
-  if [ "$IN" == "" ]; then
-    local OUTPUT=$($CMD)
+  if [ "$IN" != "" ]; then
+    printf "STDIN: $IN\n"
+  fi
+
+  if [ "$ARGS" == "" ]; then
+    if [ "$IN" == "" ]; then
+      local OUTPUT=$($CMD)
+    else
+      local OUTPUT=$(echo $IN | $CMD)
+    fi
   else
-    local OUTPUT=$(echo $IN | $CMD)
+    printf "ARGS: $ARGS\n\n"
+    if [ "$IN" == "" ]; then
+      $CMD $ARGS > output.txt
+    else
+      echo $IN | $CMD $ARGS > output.txt
+    fi
+    local OUTPUT=$(cat output.txt)
+    rm output.txt
   fi
 
   cd $OLD_PWD
@@ -34,15 +50,15 @@ run_test() {
   fi
 }
 
-run_test "with_default" "${SBT_EXEC}" "hello, world"
+run_test "with_default" "${SBT_EXEC}" "" "hello, world"
 
-run_test "with_default" "${SBT_EXEC} shell" "> exit" "exit"
+run_test "with_default" "${SBT_EXEC}" "shell" "> exit" "exit"
 
-run_test "with_default" "${SBT_EXEC} foo" "Not a valid command: foo"
+run_test "with_default" "${SBT_EXEC}" "foo" "Not a valid command: foo"
 
 # run Mac tests
 if [[ "$(uname)" == "Darwin" ]]; then
   # simulate a non-interactive open by running this from the home dir
-  run_test "$HOME" "${SBT_EXEC}" "Not a valid command: default"
-  run_test "$HOME" "${SBT_EXEC}" "Set current project to sbt-launcher-default"
+  run_test "$HOME" "${SBT_EXEC}" "" "Not a valid command: default"
+  run_test "$HOME" "${SBT_EXEC}" "" "Set current project to sbt-launcher-default"
 fi
